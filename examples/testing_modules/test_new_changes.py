b781e2f8dd54a4c80d64b1cc7bc22b2fead612f8
adiramani <adiramani84@gmail.com>
2024-05-26 23:27:27 +0100

    Small updates to fix bugs

--- examples/testing_modules/test_new_changes.py
+++ examples/testing_modules/test_new_changes.py
@@ -58,16 +58,16 @@ def run_sim(
         add_state_to_run_data(
             state,
             run_data=run_data_age,
-            number=True,
-            n_treatments=True,
-            achieved_coverage=True,
+            number=False,
+            n_treatments=False,
+            achieved_coverage=False,
             with_age_groups=True,
             prevalence=True,
-            mean_worm_burden=True,
-            prevalence_OAE=True,
-            intensity=True,
-            with_sequela=True,
-            with_pnc=True,
+            mean_worm_burden=False,
+            prevalence_OAE=False,
+            intensity=False,
+            with_sequela=False,
+            with_pnc=False,
         )
     # testing saving to hdf5 for first 2 files
     if i <= 2:
